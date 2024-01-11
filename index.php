dd94744a5b52ad2580c8911d03ce677a6704aa9f
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-01-11 14:23:39 +0100

    feat: struttura

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+    <div id="app">
+    </div>
+    <!-- axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- fine axios -->
+    <!-- vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- fine vue -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
